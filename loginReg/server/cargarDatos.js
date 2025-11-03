0e248bc850db9c3302196975717b10b0afff353d
new-CarlosEst <c.estebandiez@gmail.com>
2025-11-03 21:14:44 +0100

    Creado cargarDatos.js y comenzada la funcion para el login
    En cargar datos me cargo datos siempre y cuando no haya datos previamente cargados
    
    creada la funcion para ver si la contraseña y el correo estan dentro de la base de datos y asi poder hacer login

---
+++ loginReg/server/cargarDatos.js
@@ -0,0 +1,41 @@
+//Archivo en el que cargare los datos a la base de datos
+
+//Me importo la conexion y los drivers para encriptar datos
+import { conectar, conectarse } from "./conexion.js";
+import bcrypt from "bcrypt";
+
+//Me exporto la funcion de cargar datos
+export async function cargarDatos() {
+    //Sacamos la conexion a la db usando await ya que es una funcion asincrona
+    //El await este lo que indice es que esperara a que en el metodo conectarse se conecta a la bd de mongodb
+    const conexion = await conectarse();
+
+    //me conecto a la coleccio/tabla que voy a usar, en este caso usuario
+    const coleccion = conexion.collection("usuario");
+
+    //Verifico si hay datos en la coleccion, si no hay datos cargare los datos a la db
+    const cantidad = await coleccion.countDocuments();
+    if (cantidad > 0) {
+        console.log("La coleccion usuario ya tiene datos, asi que no se cargaran de nuevo");
+        return; //Salgo de la funcion
+    }
+    else {
+        const usuariosCargar = [
+            {nombre: "Carlos", email : "carlosprueba@gmail.com", password: "123A"},
+            {nombre: "Pilar", email: "pilarprueba@gmail.com", password: "987Z"},
+            {nombre: "Anne", email: "anneprueba@gmail.com", password: "abc2"},
+            {nombre: "Ivan", email: "ivanprueba@gmail.com", password: "987Z"}
+        ]
+
+        //Me hasheo las contraseñas recorriendo el array de objetos y cambiando su contraseña por esta hasheada
+        for (let usuario of usuariosCargar){
+            //La hasheo 10 veces
+            usuario.password = await bcrypt.hash(usuario.password, 10);
+        }
+
+        //Me cargo los datos 
+        const carga = await coleccion.insertMany(usuariosCargar);
+        //Muestro cuantos inserts se hicieron
+        console.log("Numero de inserciones: " + carga.insertedCount())
+    }
+}
--- loginReg/server/funciones.js
+++ loginReg/server/funciones.js
@@ -1,1 +1,10 @@
 //Clase en la que me creare las funciones de sacar e insertar datos a la db
+
+//Me importo todo lo necesario
+import { conectarse } from "./conexion";
+import bcrypt from "bcrypt";
+
+//funcion que recibe un email y una contraseña y los un objeto con los datos
+export async function sacarDatos (email , password) {
+    //Me conecto
+}
