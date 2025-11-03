4fefd831c156873fa475293f1876ee5f6fcbe09b
new-CarlosEst <c.estebandiez@gmail.com>
2025-11-03 21:57:49 +0100

    Avance corto a sacarDatos de funciones.js

--- loginReg/server/funciones.js
+++ loginReg/server/funciones.js
@@ -6,5 +6,9 @@ import bcrypt from "bcrypt";
 
 //funcion que recibe un email y una contraseña y los un objeto con los datos
 export async function sacarDatos (email , password) {
-    //Me conecto
+    //Me conecto a la db y a la coleccion usuario
+    const conexion = await conectarse();
+    const coleccion = conexion.collection("usuario");
+
+    
 }
