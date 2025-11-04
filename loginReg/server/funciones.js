aefb521aa4859c00b73eed374dec52e676cff8af
new-CarlosEst <c.estebandiez@gmail.com>
2025-11-04 02:08:11 +0100

    Avance en index.js, importadas las bibliotecas de express y cors
    La biblioteca de exrepess me permite crear un pequeño servidor para recibir peticiones y el cors hace que se permita recibir peticiones de mas puertos, no solo el del servidor express
    
    En index.js me he cargado los dtos y ademas he hecho que el puerto al que escucha el servidor expresss sea el 3000

--- loginReg/server/funciones.js
+++ loginReg/server/funciones.js
@@ -1,14 +1,56 @@
-//Clase en la que me creare las funciones de sacar e insertar datos a la db
+//Archivo en la que me creare las funciones de sacar e insertar datos a la db
 
 //Me importo todo lo necesario
 import { conectarse } from "./conexion";
 import bcrypt from "bcrypt";
 
+//Me conecto a la db y a la coleccion usuario
+const conexion = await conectarse();
+const coleccion = conexion.collection("usuario");
+
 //funcion que recibe un email y una contraseña y los un objeto con los datos
 export async function sacarDatos (email , password) {
-    //Me conecto a la db y a la coleccion usuario
-    const conexion = await conectarse();
-    const coleccion = conexion.collection("usuario");
+    try {
+        //Busco a un usuario por email
+        const usuario = await coleccion.findOne({email: email});
 
-    
+        //Si el usuario con ese email no existe devuelvo falso
+        if (!usuario){
+            return false;
+        }
+        //si existe
+        else {
+            //Compruebo la contraseña, y uso bycrypt ya que la de la db esta hasheada
+            const comprobarContra = await bcrypt.compare(password, usuario.password);
+            
+            //Si la contraseña no coincide
+            if (!comprobarContra){
+                return false;
+            }
+            else {
+                return usuario;
+            }
+        }
+    } catch (error){
+        console.error ("Error al consultar datos\n" + error);
+        return false;
+    }
+}
+
+//funcion que recibe un email, contraseña e usuario y la mete en la db
+export async function insertarDatos (email, user, password){
+    try {
+            //Hago el insert
+            const insertar = await coleccion.insertOne({
+                nombre: user,
+                email: email,
+                password : await bcrypt.hash(password, 10)
+            });
+
+            //si fue exitoso devuelve acknowledge que sera true
+            return insertar.acknowledged;
+    } catch (error){
+        console.error("Error al insersertar en la base de datos\n" + error);
+        return false;
+    }
 }
--- loginReg/server/index.js
+++ loginReg/server/index.js
@@ -1,2 +1,56 @@
-import { conectarse } from "./conexion";
+//Me importo todos mis archivos .js
+import { sacarDatos, insertarDatos } from "./funciones";
+import { cargarDatos } from "./cargarDatos";
 
+//Me importo los drivers de corps y express
+import express from "express";
+import cors from "cors";
+
+//crea una instacia express para crear un servidor donde se van a recibir las peticiones (tipo apache para php)
+const app = express();
+//Esto hace que se puede recibir peticiones desde otros servidores diferentes al que tenga el servidor express, asi no lo bloquee
+app.use(cors());
+//Esto hace que los datos que se me envian desde cliente al servidor vengan en formato json para manejarlo mejor
+app.use(express.json());
+
+//Me cargo los datos 
+(async () => {
+    try{
+        //LLamo a la funcion con await
+        await cargarDatos();
+        console.log("Exito al invocar la funcion cargarDatos");
+    } catch (error) {
+        console.error("Error al invocar la funcion cargarDatos\n" + error);
+    }
+})(); //Los parentesis vacios estos ultimos es para llamar a la funcion anonima que he creado para cargar datos
+
+//* Peticion de registro
+//Me hago una peticion con /register sobre el localhost:3000 que recibe una peticion y una respuesta
+//La peticion son los datos que recibo y la respuesta lo que devuelvo
+app.post("/register", async (peticion, respuesta) =>{
+    try {
+        //Me saco los datos del cuerpo de la peticion suponiendo que son usuario, email y contraseña y lo meto en tres variables
+        const {user, email, password} = peticion.body;
+
+        //Compruebo si el usuario y contraseña estan en la db
+        const estanRegistrados = sacarDatos(email, password);
+        //si devuelve falso, es que no estan registrados asi que continuo
+        if (!estanRegistrados){
+
+        }
+        //Si no es que si estan registrados asi que paro la ejecucion
+        else {
+            //TODO Mirar pq creo que la respuesta no deberia ser asi y terminar la peticion de registro
+            //Devuelvo un objeto diciendo que no se ha registrado y pq 
+            //respuesta.json({ ok: true, mensaje: "Usuario registrado correctamente" });
+        }
+    } catch (error){
+
+    }
+
+});
+
+//Pongo el servidor express en el puerto 3000
+app.listen(3000, () => {
+    console.log("Servidor corriendo en puerto 3000");
+});
